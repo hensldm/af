1432c2992abf2b4ad743c146f5a09dce5e5fb8ef
Derek Hensley <hensley.derek58@gmail.com>
2025-01-19 09:51:46 -0800

    YAML Setup

---
+++ src/code/m_flashrom.c
@@ -0,0 +1,87 @@
+#include "m_flashrom.h"
+#include "global.h"
+
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008ECA0_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008ECC8_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008ECF0_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008ED14_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008EE24_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008EE5C_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008EE6C_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008EE7C_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008EEB4_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008EEE8_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008EF0C_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/mFRm_CheckSaveData.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/mFRm_ClearSaveCheckData.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008EFDC_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008F020_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008F040_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008F0A0_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008F1BC_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008F210_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008F23C_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008F24C_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008F530_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008F5FC_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008F648_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008F768_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008F7C8_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008F8A0_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008F938_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008F968_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008FA28_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008FA50_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008FA74_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008FAB4_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008FAE0_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008FB64_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008FBEC_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008FCE8_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008FDD4_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008FE74_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_8008FF60_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_80090044_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_flashrom/func_800900B0_jp.s")
